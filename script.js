d042e2cac0dc7a702643c5a193a42aaa268431d9
vyankit <96366955+vyankit@users.noreply.github.com>
2021-12-20 17:05:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var amount = parseFloat(principal)+parseFloat(interest);
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
         
